0b90be38b395a9a7998a32e03b1c55917ad59920
Viipaxx <kngatuno@gmail.com>
2023-04-06 21:53:26 -0300

    docs: atualiza o documento index.js

--- build/index.js
+++ build/index.js
@@ -1,5 +1,14 @@
 "use strict";
 // tipos primitivos
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 let nome = "Vitor";
 let isHomem = true;
 let idade = 30;
@@ -40,3 +49,9 @@ function callToPhone(phone) {
     return phone;
 }
 console.log(callToPhone("81997819213"));
+// funções async
+function getDatabase(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return "Vitor";
+    });
+}
